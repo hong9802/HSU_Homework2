2ede383021e03046b5397761cc5d45b86cd0a8ea
HongHyeon Lee <hlee993@gmail.com>
2022-05-08 00:12:10 +0900

    Week Calendar Add...

--- app/src/main/java/com/hansung/android/myapplication2/MainActivity.java
+++ app/src/main/java/com/hansung/android/myapplication2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.hansung.android.myapplication2;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,21 +7,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import java.util.Calendar;
-
 public class MainActivity extends AppCompatActivity {
-    ActionBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Calendar today = Calendar.getInstance();
-        int year = today.get(Calendar.YEAR);
-        int month = today.get(Calendar.MONTH) + 1;
-        String now = year + "년" + month + "월";
-        bar = getSupportActionBar();
-        if(bar != null)
-            bar.setTitle(now);
     }
 
     @Override
@@ -35,12 +24,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //Item 선택 처리
         switch (item.getItemId()) {
-            default:
-                return super.onOptionsItemSelected(item);
+            case R.id.week_set:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment, new WeekFragment(this)).commit();
+                break;
         }
+        return super.onOptionsItemSelected(item);
     }
 }
-/*
-오버플로우 메뉴 처리 : https://kwanulee.github.io/AndroidProgramming/navigation/actionbar.html
-AppBar Title 변경 : https://milkissboy.tistory.com/24
- */
